8f2660982f18b961aa78a7d1b5b59eb43dab87fe
Vedansh <vedanshsingour@gmail.com>
2022-08-17 00:40:41 +0530

    login system
    adding login and signup system

--- home_page.java
+++ home_page.java
@@ -1,5 +1,28 @@
-public class home_page {
+import java.util.*;
+
+public class home_page extends Thread {
     public static void main(String[] args) {
-        System.out.println("\t Welcome to HCL Fly Ways");
+        Scanner scam = new Scanner(System.in);
+
+        System.out.println("\t ...........Welcome to HCL Fly Ways...........");
+        System.out.println();
+        System.out.println("\t\t\t .....Login.....");
+        System.out.println("Press Enter to login :)");
+        System.out.println("Press r to register yourself");
+        
+        String a = scam.nextLine();
+        switch (a) {
+            case "r":
+            case "R":
+                // call register function 
+                break;
+            default:
+                // call login function
+                break;
+        }
+    }
+    static void lomgin() {
+        System.out.println("Hemlo");
+        
     }
 }
