70a161f3998b4b1535354ec2dcc2a294b45fc9a3
Vedansh <vedanshsingour@gmail.com>
2022-08-17 00:41:38 +0530

    .

---
+++ Rubbish/2022-08-17 00.35.32.00/Main.java
@@ -0,0 +1,71 @@
+import java.sql.Connection;
+import java.sql.*;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Scanner sc = new Scanner(System.in);
+        List<Flight_info> info = new ArrayList<>();
+        Flight_infoDAO obj = new Flight_infoDAO();
+        System.out.println("--------AIRLINE MANAGEMENT SYSTEM--------");
+        System.out.println("WELCOME TO HCL AIRLINE");
+        System.out.println("ENTER 1 USER LOGIN ");
+        System.out.println("ENTER 2 ADMIN LOGIN ");
+        int ch1 = sc.nextInt();
+        switch (ch1) {
+            case 1: {
+                System.out.println("ENTER THE CREDIANTIALS");
+                String u_name = sc.nextLine();
+                System.out.println("ENTER PASSWORD : ");
+                String pass = sc.nextLine();
+                Connection conn = DBConnection.getConnection();
+                String q = ("select * from login where username='" + u_name + "'");
+                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+                ResultSet rs = stmt.executeQuery(q);
+                rs.next();
+                String U = rs.getString(1);
+                String P = rs.getString(2);
+                // System.out.println(U+" "+P);
+
+                if (pass.equals(P)) {
+                    System.out.println("----------WELCOME TO HCL AIRLINE----------");
+                    System.out.println("ENTER 1 FOR FLIGHT TICKET BOOKING");
+                    System.out.println("ENTER 2 FOR TICKET CANCELLATION");
+                    int ch = sc.nextInt();
+                    switch (ch) {
+                        case 1:
+                             System.out.println("");
+                            
+                            break;
+                        case 2:
+
+                            break;
+
+                        default:
+                            System.out.println("INVALID CHOICE");
+                            break;
+                    }
+
+                } else {
+                    System.out.println("INVALID PASSWORD TRY AGAIN");
+                }
+                break;
+            }
+            case 2:{
+                String admin_username;
+                String admin_password;
+                System.out.println("ENTER THE ADMIN_USERNAME");
+                admin_username=sc.nextLine();
+                System.out.println("ENTER THE ADMIN_PASSWORD");
+                admin_password=sc.nextLine();
+            }
+
+        }
+
+    }
+}
